5a1278fba1f3045195ea5b61969ab1b924f6b404
Iván Ortiz Martín <ivan@transgruasortiz.com>
2017-05-31 15:48:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 31 11:07:35 2017
-
-@author: iv
-"""
-
 from distutils.core import setup
 setup(
   name = 'ciff_2017_af3_asio',
@@ -13,8 +6,8 @@ setup(
   description = 'oportunidad de subida Andres Salamanca e Ivan Ortiz',
   author = 'Andres Salamanca e Ivan Ortiz',
   author_email = 'ivanortiz@campusciff.net',
-  url = 'https://github.com/ivtransgruasortiz/paquete_pip', # use the URL to the github repo
+  url = 'https://github.com/ivtransgruasortiz/paquete_pip/', # use the URL to the github repo
   download_url = 'https://github.com/ivtransgruasortiz/paquete_pip/tarball/0.1',
-  keywords = ['testing', 'logging', 'example'],
+  keywords = ['paquete', 'oportunidaddesubida'],
   classifiers = [],
 )

179a9a64d7ad9be182b2ddf6443badda7793c553
Iván Ortiz Martín <ivan@transgruasortiz.com>
2017-06-01 11:23:36 +0200

    Update var.py

--- ciff_2017_af3_asio/var.py
+++ ciff_2017_af3_asio/var.py
@@ -7,7 +7,7 @@ def var(p,c,ticker):
     import numpy as np
     import pandas as pd
     import json
-    import pygal
+    #import pygal
     import matplotlib.pyplot as plt
     from scipy.stats import norm
     from bokeh.charts import Histogram

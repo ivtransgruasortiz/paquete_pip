ba363edb82e35ed7c0c4abced26c74cc2fd38367
Iván Ortiz Martín <ivan@transgruasortiz.com>
2017-06-01 10:41:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ciff_2017_af3_asio',
   packages = ['ciff_2017_af3_asio'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'oportunidad de subida Andres Salamanca e Ivan Ortiz segunda prueba',
   author = 'Andres Salamanca e Ivan Ortiz',
   author_email = 'ivanortiz@campusciff.net',
